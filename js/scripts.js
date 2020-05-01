098359f5141f9648d2b4a178a9d8a6a8de8478c7
nikikokkinos <nk1942@nyu.edu>
2020-05-01 14:04:33 -0400

    3 geojson layers added, image symbol added for hospital point, layerToggle check input added

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,91 @@ var map = new mapboxgl.Map({
   minZoom: 12,
   center: [-73.915355,40.771302],
 })
+
+var busUrl = 'https://raw.githubusercontent.com/nikikokkinos/Data/master/CD22BusRoutes.geojson'
+var truckUrl = 'https://raw.githubusercontent.com/nikikokkinos/Data/master/CD22TruckRoutes.geojson'
+var hospitalUrl = 'https://raw.githubusercontent.com/nikikokkinos/Data/master/CD22Hospital.geojson'
+
+map.on('load', function() {
+
+  map.addSource('bus', {
+    'type': 'geojson',
+    'data': busUrl,
+    'generateId': true
+  })
+
+  map.addSource('truck', {
+    'type': 'geojson',
+    'data': truckUrl,
+    'generateId': true
+  })
+
+  map.loadImage('https://upload.wikimedia.org/wikipedia/commons/c/ce/Flag_of_the_Red_Cross_cropped_as_a_square.png',
+
+    function(error, image) { if (error) throw error;
+
+      map.addImage('cross', image)
+
+      map.addSource('hospital', {
+      'type': 'geojson',
+      'data': hospitalUrl,
+      'generateId': true
+    })
+
+    map.addLayer({
+      'id': 'busCD22',
+      'type': 'line',
+      'source': 'bus',
+      'layout': {
+        'line-join': 'round',
+        'line-cap': 'round',
+        'visibility': 'visible',
+        },
+      'paint': {
+          'line-color': 'blue',
+          'line-width': 3,
+        },
+    })
+
+    map.addLayer({
+      'id': 'truckCD22',
+      'type': 'line',
+      'source': 'truck',
+      'layout': {
+        'line-join': 'round',
+        'line-cap': 'round',
+        'visibility': 'visible',
+        },
+      'paint': {
+          'line-color': 'black',
+          'line-width': 3,
+        },
+    })
+
+
+    map.addLayer({
+      'id': 'hospitalCD22',
+      'type': 'symbol',
+      'source': 'hospital',
+      'layout': {
+        'icon-image': 'cross',
+        'icon-size': 0.05
+        }
+      })
+
+    })
+
+  var radioButton = $('#layerToggle')
+
+    radioButton.on("click", function () {
+      if (document.getElementById('MountSinaiQueens').checked) {
+          map.setLayoutProperty('hospitalCD22', 'visibility', 'visible')
+      } else { map.setLayoutProperty('hospitalCD22', 'visibility', 'none')
+    } if (document.getElementById('BusRoutes').checked) {
+          map.setLayoutProperty('busCD22', 'visibility', 'visible')
+      } else { map.setLayoutProperty('busCD22', 'visibility', 'none')
+    } if (document.getElementById('TruckRoutes').checked) {
+          map.setLayoutProperty('truckCD22', 'visibility', 'visible')
+      } else { map.setLayoutProperty('truckCD22', 'visibility', 'none')}
+    })
+})
